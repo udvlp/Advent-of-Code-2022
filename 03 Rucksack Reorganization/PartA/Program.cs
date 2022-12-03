611da1a4e1ec192e6912fa5faeeadc8ce1a65a3a
Ulf Haueisen <ulf@dvlp.de>
2022-12-04 00:27:40 +0100

    Day 03

---
+++ 03 Rucksack Reorganization/PartA/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\..\input.txt");
+            int sum = 0;
+            while (!sr.EndOfStream)
+            {
+                var set = new HashSet<char>();
+                string? l = sr.ReadLine();
+                for (int i = 0; i < l.Length / 2; i++)
+                {
+                    set.Add(l[i]);
+                }
+                for (int i = l.Length / 2; i < l.Length; i++)
+                {
+                    if (set.Contains(l[i]))
+                    {
+                        if (l[i] >= 'a' && l[i] <= 'z')
+                        {
+                            sum += l[i] - 'a' + 1;
+                        }
+                        else if (l[i] >= 'A' && l[i] <= 'Z')
+                        {
+                            sum += l[i] - 'A' + 27;
+                        }
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 03 Rucksack Reorganization/PartB/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\..\input.txt");
+            int sum = 0;
+            while (!sr.EndOfStream)
+            {
+                HashSet<char>? set = null;
+                for (int g = 0; g < 3; g++)
+                {
+                    string? l = sr.ReadLine();
+                    HashSet<char> lineset = new();
+                    for (int i = 0; i < l.Length; i++)
+                    {
+                        lineset.Add(l[i]);
+                    }
+                    if (set == null)
+                    {
+                        set = lineset;
+                    }
+                    else
+                    {
+                        set.IntersectWith(lineset);
+                    }
+                }
+                foreach (char c in set)
+                {
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        sum += c - 'a' + 1;
+                    }
+                    else if (c >= 'A' && c <= 'Z')
+                    {
+                        sum += c - 'A' + 27;
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
