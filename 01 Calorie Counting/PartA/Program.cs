1eb9290aa724c25c053e4750b6a143479ba225bd
Ulf Haueisen <ulf@dvlp.de>
2022-12-04 00:27:20 +0100

    Day 01

---
+++ 01 Calorie Counting/PartA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\..\input.txt");
+            int sum = 0;
+            int max = 0;
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if (line == "")
+                {
+                    if (sum > max) max = sum;
+                    sum = 0;
+                }
+                else
+                {
+                    sum += int.Parse(line);
+                }
+            }
+            if (sum > max) max = sum;
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ 01 Calorie Counting/PartB/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\..\input.txt");
+            int sum = 0;
+            List<int> cal = new List<int>();
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if (line == "")
+                {
+                    cal.Add(sum);
+                    sum = 0;
+                }
+                else
+                {
+                    sum += int.Parse(line);
+                }
+            }
+            if (sum > 0) cal.Add(sum);
+            cal.Sort((a, b) => b.CompareTo(a));
+            Console.WriteLine(cal[0] + cal[1] + cal[2]);
+        }
+    }
+}
