46b18e79701f26a1dede32c3c2e470de85bb3de7
Ulf Haueisen <ulf@dvlp.de>
2022-12-04 10:12:36 +0100

    Day 04

---
+++ 04 Camp Cleanup/PartA/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\..\input.txt");
+            int count = 0;
+            while (!sr.EndOfStream)
+            {
+                string? line = sr.ReadLine();
+                int[] a = line.Split('-', ',').Select(Int32.Parse).ToArray();
+                if (a[0] >= a[2] && a[1] <= a[3] || a[2] >= a[0] && a[3] <= a[1])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ 04 Camp Cleanup/PartB/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\..\input.txt");
+            int count = 0;
+            while (!sr.EndOfStream)
+            {
+                string? line = sr.ReadLine();
+                int[] a = line.Split('-', ',').Select(Int32.Parse).ToArray();
+                if (a[0] > a[2])
+                {
+                    int t = a[0]; a[0] = a[2]; a[2] = t;
+                    t = a[1]; a[1] = a[3]; a[3] = t;
+                }
+                if (a[2] <= a[1])
+                {
+                    // Console.WriteLine(Math.Min(a[1], a[3]) - a[2] + 1);
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
