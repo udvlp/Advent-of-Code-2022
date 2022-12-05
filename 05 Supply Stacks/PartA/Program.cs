5c8445b825f7c3381e8174611c503b4a91d5ed05
Ulf Haueisen <ulf@dvlp.de>
2022-12-05 21:15:01 +0100

    Day 05

---
+++ 05 Supply Stacks/PartA/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(@"..\..\..\input.txt");
+
+            List<string> lines = new();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (line == "") break;
+                lines.Add(line);
+            }
+            int numstacks = (lines[lines.Count - 1].Length + 2) / 4;
+
+            var stacks = new Stack<char>[numstacks];
+            for (int i = 0; i < numstacks; i++)
+            {
+                stacks[i] = new Stack<char>();
+            }
+
+            for (int i = lines.Count - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < numstacks; j++)
+                {
+                    int x = j * 4 + 1;
+                    if (x < lines[i].Length && lines[i][x] != ' ')
+                    {
+                        stacks[j].Push(lines[i][x]);
+                    }
+                }
+            }
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                var parts = line.Split(' ');
+                if (parts[0] == "move")
+                {
+                    int cnt = int.Parse(parts[1]);
+                    int src = int.Parse(parts[3]) - 1;
+                    int dst = int.Parse(parts[5]) - 1;
+                    for (int i = 0; i < cnt; i++)
+                    {
+                        stacks[dst].Push(stacks[src].Pop());
+                    }
+                }
+            }
+
+            for (int j = 0; j < numstacks; j++)
+            {
+                Console.Write(stacks[j].Peek());
+            }
+            Console.WriteLine();
+        }
+    }
+}
