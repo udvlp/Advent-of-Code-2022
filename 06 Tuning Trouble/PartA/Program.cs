4067a642092d8002ca1e5b7af796bb933907e044
Ulf Haueisen <ulf@dvlp.de>
2022-12-06 11:43:30 +0100

    Day 06

---
+++ 06 Tuning Trouble/PartA/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\..\input.txt");
+            string line = sr.ReadLine();
+            for (int i = 3; i < line.Length; i++)
+            {
+                if (line[i] != line[i - 1]
+                    && line[i] != line[i - 2]
+                    && line[i] != line[i - 3]
+                    && line[i - 1] != line[i - 2]
+                    && line[i - 1] != line[i - 3]
+                    && line[i - 2] != line[i - 3])
+                {
+                    Console.WriteLine(i+1);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 06 Tuning Trouble/PartB/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int len = 14;
+            var sr = new StreamReader(@"..\..\..\input.txt");
+            string line = sr.ReadLine();
+            for (int i = len - 1; i < line.Length; i++)
+            {
+                bool diff = true;
+                for (int j = i; j > i - len; j--)
+                {
+                    for (int k = j - 1; k > i - len; k--)
+                    {
+                        if (line[j] == line[k])
+                        {
+                            diff = false;
+                            break;
+                        }
+                    }
+                    if (!diff) break;
+                }
+                if (diff)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+        }
+    }
+}
