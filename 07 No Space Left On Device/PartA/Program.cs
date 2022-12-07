6f18ca03357e60c865b9c70cb4c93b4d1cc71227
Ulf Haueisen <ulf@dvlp.de>
2022-12-07 12:18:14 +0100

    Day 07

---
+++ 07 No Space Left On Device/PartA/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            int atmost100000 = 0;
+            recurse();
+            Console.WriteLine(atmost100000);
+
+            int recurse()
+            {
+                int result = 0;
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] parts = line.Split(' ');
+                    if (parts.Length > 0)
+                    {
+                        if (parts[0] == "$")
+                        {
+                            if (parts[1] == "cd")
+                            {
+                                if (parts[2] == "..")
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    result += recurse();
+                                }
+                            }
+                            else if (parts[1] == "ls")
+                            {
+                                // unused
+                            }
+
+                        }
+                        else if (parts[0] == "dir")
+                        {
+                            // Dir
+                            // unused
+                        }
+                        else
+                        {
+                            // File
+                            result += int.Parse(parts[0]);
+                        }
+                    }
+                }
+                if (result <= 100000) atmost100000 += result;
+                return result;
+            }
+        }
+    }
+}
---
+++ 07 No Space Left On Device/PartB/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            List<int> dirsizes = new();
+            const int total = 70000000;
+            const int needed = 30000000;
+            int used = recurse();
+            int free = total - used;
+            dirsizes.Sort();
+            foreach (int n in dirsizes)
+            {
+                if (n > needed-free)
+                {
+                    Console.WriteLine(n);
+                    break;
+                }
+            }
+
+            int recurse()
+            {
+                int result = 0;
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] parts = line.Split(' ');
+                    if (parts.Length > 0)
+                    {
+                        if (parts[0] == "$")
+                        {
+                            if (parts[1] == "cd")
+                            {
+                                if (parts[2] == "..")
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    result += recurse();
+                                }
+                            }
+                            else if (parts[1] == "ls")
+                            {
+                                // unused
+                            }
+
+                        }
+                        else if (parts[0] == "dir")
+                        {
+                            // Dir
+                            // unused
+                        }
+                        else
+                        {
+                            // File
+                            result += int.Parse(parts[0]);
+                        }
+                    }
+                }
+                dirsizes.Add(result);
+                return result;
+            }
+        }
+    }
+}
