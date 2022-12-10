57ca5603dfdbbcd1387e56e7bd4af41b9f34fa31
Ulf Haueisen <ulf@dvlp.de>
2022-12-10 17:49:33 +0100

    Day 09

---
+++ 09 Rope Bridge/PartA/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, (int dx, int dy)> directions = new()
+            {
+                { 'U', (0, -1) },
+                { 'R', (1, 0) },
+                { 'D', (0, 1) },
+                { 'L', (-1, 0) }
+            };
+
+            HashSet<(int x, int y)> visited = new();
+
+            var lines = File.ReadAllLines(@"..\..\input.txt");
+            var steps = lines
+               .Select(l => l.Split(' '))
+               .Select(a => (direction: a[0][0], count: int.Parse(a[1])));
+
+            (int x, int y) head = (0, 0);
+            (int x, int y) tail = (0, 0);
+
+            visited.Add(tail);
+            foreach (var step in steps)
+            {
+                for (int i = 0; i < step.count; i++)
+                {
+                    var (dx, dy) = directions[step.direction];
+                    head = (head.x + dx, head.y + dy);
+                    if (int.Abs(head.y - tail.y) > 1 || int.Abs(head.x - tail.x) > 1)
+                    { 
+                        tail.y += int.Sign(head.y - tail.y);
+                        tail.x += int.Sign(head.x - tail.x);
+                    }
+                    visited.Add(tail);
+                }
+            }
+
+            Console.WriteLine(visited.Count);
+        }
+    }
+}
---
+++ 09 Rope Bridge/PartB/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, (int dx, int dy)> directions = new()
+            {
+                { 'U', (0, -1) },
+                { 'R', (1, 0) },
+                { 'D', (0, 1) },
+                { 'L', (-1, 0) }
+            };
+
+            HashSet<(int x, int y)> visited = new();
+
+            var lines = File.ReadAllLines(@"..\..\input.txt");
+            var steps = lines
+               .Select(l => l.Split(' '))
+               .Select(a => (direction: a[0][0], count: int.Parse(a[1])));
+
+            (int x, int y)[] node = new (int x, int y)[10];
+
+            visited.Add(node[node.Length - 1]);
+            foreach (var step in steps)
+            {
+                for (int i = 0; i < step.count; i++)
+                {
+                    var (dx, dy) = directions[step.direction];
+                    node[0] = (node[0].x + dx, node[0].y + dy);
+                    for (int t = 1; t < node.Length; t++)
+                    {
+                        if (int.Abs(node[t-1].y - node[t].y) > 1 || int.Abs(node[t-1].x - node[t].x) > 1)
+                        {
+                            node[t].y += int.Sign(node[t-1].y - node[t].y);
+                            node[t].x += int.Sign(node[t-1].x - node[t].x);
+                        }
+                    }
+                    visited.Add(node[node.Length - 1]);
+                }
+            }
+
+            Console.WriteLine(visited.Count);
+        }
+    }
+}
