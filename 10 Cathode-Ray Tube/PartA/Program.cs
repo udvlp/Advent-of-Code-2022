d1fcf4830cd24413f393f6c4c12aeba14cb7ec6b
Ulf Haueisen <ulf@dvlp.de>
2022-12-10 18:34:44 +0100

    Day 10

---
+++ 10 Cathode-Ray Tube/PartA/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static int x = 1;
+        static int cyclenumber = 1;
+        static int signalstrengthsum = 0;
+        
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                var parts = line.Split(' ');
+                if (parts[0] == "noop")
+                {
+                    cycle();           
+                }
+                else if (parts[0] == "addx")
+                {
+                    int p = int.Parse(parts[1]);
+                    for (int i = 0; i < 2; i++)
+                    {
+                        cycle();
+                    }
+                    x += p;
+                }
+            }
+            Console.WriteLine(signalstrengthsum);
+        }
+
+        static void cycle()
+        {
+            if ((cyclenumber + 20) % 40 == 0)
+            {
+                Console.WriteLine($"cycle: {cyclenumber} x: {x}");
+                signalstrengthsum += cyclenumber * x;
+            }
+            cyclenumber++;
+        }
+    }
+}
---
+++ 10 Cathode-Ray Tube/PartB/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static int x = 2;
+        static int cyclenumber = 1;
+
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                var parts = line.Split(' ');
+                if (parts[0] == "noop")
+                {
+                    cycle();
+                }
+                else if (parts[0] == "addx")
+                {
+                    int p = int.Parse(parts[1]);
+                    for (int i = 0; i < 2; i++)
+                    {
+                        cycle();
+                    }
+                    x += p;
+                }
+            }
+        }
+
+        static void cycle()
+        {
+            int h = (cyclenumber - 1) % 40 + 1;
+            if (h >= x - 1 && h <= x + 1) Console.Write("#"); else Console.Write(".");
+            if (h == 40) Console.WriteLine();
+            cyclenumber++;
+        }
+    }
+}
